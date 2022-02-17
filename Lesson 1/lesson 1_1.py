34cb56b2206ef025a3dcdf9a5e89db0e0c0407d3
filanor <filanor@gmail.com>
2022-02-17 12:56:17 +0300

    Create lesson 1_1.py

---
+++ Lesson 1/lesson 1_1.py
@@ -0,0 +1,17 @@
+# ЗАДАЧА
+# Выведите на экран
+# Glo Academy
+#
+# Glo Academy
+# Сделайте это разными способами:
+# 1. с помощью трех команд print
+# 2. с помощью одной команды print
+
+
+# РЕШЕНИЕ
+#print('Glo Academy\n\nGlo Academy')
+print('Glo Academy', 'Glo Academy', sep='\n\n')
+
+print('Glo Academy')
+print()
+print('Glo Academy')
