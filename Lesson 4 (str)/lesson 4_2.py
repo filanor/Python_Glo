2b5847d2f255d3509a019d973588c81070b1ee8c
filanor <filanor@gmail.com>
2022-02-22 11:05:42 +0300

    Add files via upload

---
+++ Lesson 4 (str)/lesson 4_2.py
@@ -0,0 +1,8 @@
+# ЗАДАЧА
+# нужно узнать, есть ли в этом посте упоминание об Glo Academy.
+
+# РЕШЕНИЕ
+
+text = input('Введите текст: ')
+answer = 'YES' if 'Glo Academy' in text else 'NO'
+print(answer)
